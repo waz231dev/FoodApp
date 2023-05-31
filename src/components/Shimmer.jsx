dd84a5f418ace7ae21b19c000f5e5a2e319c3340
Wasim Akram <akramdel231@gmail.com>
2023-05-31 12:53:15 +0530

    Added Shimmer UI

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,6 +1,9 @@
 const Shimmer = () => {
     return (
-        <h1>Shimmer UI Loading...</h1>
+        <div className='restaurant-list flex flex-wrap gap-4'>
+            {Array(10).fill("").map((item, index) => (<div key={index} className="card w-52 h-64 border-2 bg-gray-300 p-2 m-4 rounded-lg"></div>))}
+
+        </div>
     )
 }
 
